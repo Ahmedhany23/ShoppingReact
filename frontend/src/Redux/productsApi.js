09cdd399603f962c9403e81506822ebcc4502107
Ahmed Hany Eldesoky <ahmedhany.22@hotmail.com>
2024-02-27 12:20:14 +0200

    productsApi.js

--- frontend/src/Redux/productsApi.js
+++ frontend/src/Redux/productsApi.js
@@ -1,27 +1,27 @@
 // Need to use the React-specific entry point to import createApi
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
 export const productsApi = createApi({
-  reducerPath: 'productsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+  reducerPath: "productsApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://shoppingreact.onrender.com/" }),
   endpoints: (builder) => ({
     getproductsByName: builder.query({
       query: (name) => `products`,
     }),
   }),
-})
+});
 export const OneproductApi = createApi({
-  reducerPath: 'OneproductApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+  reducerPath: "OneproductApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://shoppingreact.onrender.com/" }),
   endpoints: (builder) => ({
     getOneProduct: builder.query({
       query: (name) => `products/${name}`,
     }),
   }),
-})
+});
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetproductsByNameQuery } = productsApi
-export const { useGetOneProductQuery } = OneproductApi
+export const { useGetproductsByNameQuery } = productsApi;
+export const { useGetOneProductQuery } = OneproductApi;
